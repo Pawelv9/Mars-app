b1bc9e55ae25c8e48c1894cff3a93ec5155746ca
Pavel <Pawelv9@gmail.com>
2019-04-21 01:28:12 +0300

    fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ name, email, id }) => {
     return ( 
         <div className = 'bg-dark-blue dib br3 pa3 ma2 grow bw2 shadow-5' >
             <img alt = 'robots'
-            src = { `https://robohash.org/${id}?200x200` }/> 
+            src = { `https://robohash.org/${id}?size=200x200` }/> 
         <div>
             <h2 > { name } </h2> 
             <p> { email } </p>         
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -5,12 +5,12 @@ const CardList = ({ robots }) => {
 
     return ( 
         <div > {
-            robots.map((user, i) => {
-                return ( <
-                    Card key = { i }
-                    id = { robots[i].id }
-                    name = { robots[i].name }
-                    email = { robots[i].email }
+            robots.map((robot) => {
+                return ( <Card 
+                    key = { robot.id }
+                    id = { robot.id }
+                    name = { robot.name }
+                    email = { robot.email }
                     /> 
                 );
             })
